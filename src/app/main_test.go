ec5d791f5b2aa26302b3553dbad8dc43825f9f4d
Alley M. Carvalho <alleycarvalho@gmail.com>
2019-08-31 04:15:47 -0300

    Corrigindo teste

--- src/app/main_test.go
+++ src/app/main_test.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	// "math"
 	"testing"
 )
 
 func TestCalculo(t *testing.T) {
-	// got := math.Sqrt(49)
-	got := 7
-	want := 7
+	got := "7"
+	want := "7"
 
 	if got != want {
 		t.Errorf("Raiz quadrada inválida: obteve %s e requer %s", got, want)
